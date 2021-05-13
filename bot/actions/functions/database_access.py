e7ca2f935b8579faadd540f3e4162a388845770b
FRANZKAFKA13 <carstengranig@gmail.com>
2021-05-13 22:46:05 +0200

    add basic DB functions to insert users and subscriptions

--- bot/actions/functions/database_access.py
+++ bot/actions/functions/database_access.py
@@ -8,10 +8,8 @@ import pandas as pd
 
 def get_subscribed_artist(user_name):
     """A function returning artists user subscribed to from the database.
-
     Args:
         user_name (String): The user's name.
-
     Returns:
         artist_uri_list: A list of URIs of subscribed artists.
     """
@@ -26,10 +24,8 @@ def get_subscribed_artist(user_name):
 
 def create_connection(db_file):
     """ Create a database connection to the SQLite database specified by db_file
-
     Args:
         db_file: database file
-    
     Returns:
         conn: Connection object or None
     """
@@ -49,15 +45,16 @@ def create_initial_db(db_file):
     cur = conn.cursor()
 
     sql_create_users_table =              """ CREATE TABLE "users" (
-                                              "user_id"	INTEGER NOT NULL UNIQUE,
-                                              "name"	INTEGER,
-                                              PRIMARY KEY("user_id")
+                                              "user_uri" INTEGER NOT NULL UNIQUE,
+                                              "name"	 INTEGER,
+                                              PRIMARY KEY("user_uri")
                                               ); """
 
     sql_create_subscribed_artists_table = """ CREATE TABLE "subscribed_artists" (
-	                                          "user_id"	INTEGER NOT NULL,
+	                                          "user_uri"	INTEGER NOT NULL,
 	                                          "artist_uri"	TEXT NOT NULL,
-                                              FOREIGN KEY(user_id) REFERENCES users(user_id)
+                                              FOREIGN KEY(user_uri) REFERENCES users(user_uri),
+                                              UNIQUE(user_uri, artist_uri)
                                               ); """
 
     try:
@@ -80,8 +77,8 @@ def add_user(db_file, user_name):
     cur = conn.cursor()
 
     # Generate new user ID by iterating
-    sql_get_latest_user_id = """ SELECT max(user_id) FROM users"""
-    cur.execute(sql_get_latest_user_id)
+    sql_get_latest_user_uri = """ SELECT max(user_uri) FROM users"""
+    cur.execute(sql_get_latest_user_uri)
     latest_id = cur.fetchall()[0][0]
     if latest_id is None:
         new_id = 1
@@ -90,7 +87,7 @@ def add_user(db_file, user_name):
 
     # Add new user to user table
     new_user = (new_id, user_name)
-    sql_add_new_user = """ INSERT INTO users(user_id, name)
+    sql_add_new_user = """ INSERT INTO users(user_uri, name)
                            VALUES(?,?) """
 
     try:
@@ -103,20 +100,20 @@ def add_user(db_file, user_name):
     cur.close()
 
 
-def add_subscription(db_file, user_id, artist_uri):
+def add_subscription(db_file, user_uri, artist_uri):
 
     conn = create_connection(db_file)
     cur = conn.cursor()
 
     # Add new subscription to user table
-    new_subscription = (user_id, artist_uri)
-    sql_add_new_user = """ INSERT INTO subscribed_artists(user_id, artist_uri)
+    new_subscription = (user_uri, artist_uri)
+    sql_add_new_user = """ INSERT INTO subscribed_artists(user_uri, artist_uri)
                            VALUES(?,?) """
 
     try:
-        cur.execute(sql_add_new_user, new_user)
+        cur.execute(sql_add_new_user, new_subscription)
     except Exception as e:
-        print("Failed adding entry to table users: " + str(e))
+        print("Failed adding new subscription: " + str(e))
     
 
     conn.commit()
@@ -125,3 +122,4 @@ def add_subscription(db_file, user_id, artist_uri):
 
 create_initial_db("../../data/test.db")
 add_user("../../data/test.db", "Even more user")
+add_subscription("../../data/test.db", 2, "test_uri")
